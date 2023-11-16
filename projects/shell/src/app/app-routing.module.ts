58b0c68816bc99b2f8f2430c761a40e1aa45fd54
Tomas Juhaniak <tomas.juhaniak@garwan.sk>
2023-11-16 21:14:59 +0100

    Alternative without federation.

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+    // {
+    //     path: 'flights',
+    //     loadChildren: () => import('remote/Module').then(m => m.FlightsModuleModule)
+    // },
     {
         path: 'flights',
-        loadChildren: () => import('remote/Module').then(m => m.FlightsModuleModule)
+        loadChildren: () => import('../../../remote/src/app/flights-module/flights-module.module').then(m => m.FlightsModuleModule)
     }
 ];
 
